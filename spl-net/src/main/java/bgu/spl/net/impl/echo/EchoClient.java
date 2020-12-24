a77013282960d105acaf5c17aceadb11fdd80547
jonathanhazut <jonathanhazut@gmail.com>
2020-12-24 19:16:56 +0200

    i made int n = 5;

--- spl-net/src/main/java/bgu/spl/net/impl/echo/EchoClient.java
+++ spl-net/src/main/java/bgu/spl/net/impl/echo/EchoClient.java
@@ -13,6 +13,7 @@ public class EchoClient {
 
         if (args.length == 0) {
             args = new String[]{"localhost", "hello"};
+            int n = 5;
         }
 
         if (args.length < 2) {
